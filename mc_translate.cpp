f709625cc666a68f6594f32cb5cff9759824f0da
KangboLu <redstonegranola@gmail.com>
2016-04-02 22:27:00 -0700

    add output formatting for translation
    Detect the 10th character and add a new line to it

--- mc_translate.cpp
+++ mc_translate.cpp
@@ -8,7 +8,8 @@ int main ()
     string englishRef = " abc";// English letter for reference
     string morseRef[4] = {"/", ".-","-...","-.-."};// Morse code reference
 
-    string userInput = "abc ab";// User inputs
+    string userInput = "abc a b c ab ac bc cba";// User inputs
+    int linePosit = 0;
 
     // for loop for testing purpose only
     cout << "Testing: \n";
@@ -22,18 +23,25 @@ int main ()
         else
             cout << "Not in the reference" << endl;
     }
-    cout << "------------------------\n";
+    cout << "------------------------\n\n";
 
     // for loop for basic translating purpose only
     cout << "Translating: \n";
-    for (int i = 0; i < userInput.length(); i++) {
+    for (int i = 0; i < userInput.length(); i++, linePosit++) {
 
         int findPosit = englishRef.find(userInput.at(i));
         if (findPosit+1)
             cout << morseRef[findPosit] << " ";
         else
             cout << "X" << endl;
-    }
+
+        if ((linePosit + 1) / 10) {
+            cout << "\n";
+            linePosit = (linePosit+1) % 10;
+            cout << "Line Position is: " << linePosit << endl;
+        }
+
+    }//end for loop
 
 
-}
+}//end main
