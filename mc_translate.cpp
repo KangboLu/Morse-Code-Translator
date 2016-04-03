1a06d12d3567ba31a040c5e156a26354bbfe06fd
KangboLu <redstonegranola@gmail.com>
2016-04-02 21:54:40 -0700

    Adjust format for output

--- mc_translate.cpp
+++ mc_translate.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 int main ()
 {
-    string englishRef = "abc";// English letter for reference
-    string morseRef[3] = {".-","-...","-.-."};// Morse code reference
+    string englishRef = " abc";// English letter for reference
+    string morseRef[4] = {"/", ".-","-...","-.-."};// Morse code reference
 
-    string userInput = "abc";// User inputs
+    string userInput = "abc ab";// User inputs
 
     // for loop for testing purpose only
-    cout << "Testing: \n"
+    cout << "Testing: \n";
     for (int i = 0; i < userInput.length(); i++) {
 
         cout << userInput.at(i) << " is ";
@@ -25,12 +25,12 @@ int main ()
     cout << "------------------------\n";
 
     // for loop for basic translating purpose only
-    cout << "Translating: \n"
-    for for (int i = 0; i < userInput.length(); i++) {
+    cout << "Translating: \n";
+    for (int i = 0; i < userInput.length(); i++) {
 
         int findPosit = englishRef.find(userInput.at(i));
         if (findPosit+1)
-            cout << morseRef[findPosit] << endl;
+            cout << morseRef[findPosit] << " ";
         else
             cout << "X" << endl;
     }
