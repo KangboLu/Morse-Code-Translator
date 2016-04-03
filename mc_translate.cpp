24e9a9e906e556770f4edb63202474cfbefc8fc9
KangboLu <redstonegranola@gmail.com>
2016-04-02 22:36:21 -0700

    Update comments for reviewing

--- mc_translate.cpp
+++ mc_translate.cpp
@@ -8,7 +8,7 @@ int main ()
     string englishRef = " abc";// English letter for reference
     string morseRef[4] = {"/", ".-","-...","-.-."};// Morse code reference
 
-    string userInput = "abc a b c ab ac bc cba";// User inputs
+    string userInput = "abc a b c ab ac bc cba cd addd ed ed";// User inputs
     int linePosit = 0;
 
     // for loop for testing purpose only
@@ -26,15 +26,16 @@ int main ()
     cout << "------------------------\n\n";
 
     // for loop for basic translating purpose only
-    cout << "Translating: \n";
+    cout << "Translating(English to ): \n";
     for (int i = 0; i < userInput.length(); i++, linePosit++) {
 
-        int findPosit = englishRef.find(userInput.at(i));
+        int findPosit = englishRef.find(userInput.at(i));// finded position
         if (findPosit+1)
             cout << morseRef[findPosit] << " ";
         else
             cout << "X" << endl;
 
+        // if loop for adding new line after 10th character Morse Code translation
         if ((linePosit + 1) / 10) {
             cout << "\n";
             linePosit = (linePosit+1) % 10;
