61d3c5e6fdc42f11dd28de4c270d1e6e915f1b54
KangboLu <redstonegranola@gmail.com>
2016-04-16 21:38:19 -0700

    Update morse to english section

--- mc_translate.cpp
+++ mc_translate.cpp
@@ -5,10 +5,11 @@ using namespace std;
 
 int main ()
 {
-    string englishRef = " abc";// English letter for reference
-    string morseRef[4] = {"/", ".-","-...","-.-."};// Morse code reference
+    string englishRef = " abc";// English letter for reference(for now)
+    string morseRef[4] = {"/", ".-","-...","-.-."};// Morse code reference(for now)
 
-    string userInput = "abc eeee ab cb cd ec eeee ab c eeee ab ";// User inputs
+    string userInput = "abc eeee ab cb cd ec eeee ab c eeee ab ";// user input for testing(for now)
+    string userMorseInput = ".- -... -...";// user input for testing(for now)
     int linePosit = 0;
 
     // for loop for testing purpose only
@@ -29,10 +30,10 @@ int main ()
     cout << "Translating(English to Morse): \n\n";
     for (int i = 0; i < userInput.length(); i++, linePosit++) {
 
-        // finded position by comparing englishRef and userInput
+        // Fining position by comparing englishRef and userInput
         int findPosit = englishRef.find(userInput.at(i));
 
-        //output morse code translation
+        //output Morse code translation
         if (findPosit+1)
             cout << morseRef[findPosit] << " ";
         else
@@ -43,9 +44,29 @@ int main ()
             cout << "\n";
             linePosit = (linePosit+1) % 10;// reset linePosit to 0
             cout << "Line Position is: " << linePosit << "\n\n";
-        }
+        }// end if loop
 
     }//end for loop
     cout << "\nEnd position is " << linePosit <<endl;// for testing purpose
+    cout << "------------------------\n\n";
+
+    cout << "Translating(Morse to English): \n\n";
+  //  for (int i = 0; i < userMorseInput.length(); i++) {
+        int leftPosit = 0;// begin position of the substring function call
+        int rightPosit = userMorseInput.find(" ");
+
+        cout << rightPosit << endl;
+
+        for ( int j = 0; j < 4; j++) {
+            if (userMorseInput.substr(leftPosit, rightPosit) == morseRef[j])
+                cout << englishRef.at(j);
+        }
+
+        leftPosit = rightPosit;
+        rightPosit = userMorseInput.substr(leftPosit, userMorseInput.length()-1).find(" ");
+
+        cout << rightPosit;
+
+//    }// end for loop
 
 }//end main
