e126da79c90c449e619d95fd4e6221590903b52b
KangboLu <redstonegranola@gmail.com>
2016-05-09 16:53:31 -0700

    Deleting unnecessary codes

--- mc_translate.cpp
+++ mc_translate.cpp
@@ -2,26 +2,8 @@
 
 int main ()
 {
-    // user input for testing(for now)
-
-    greeting();
-    userMenu();
-   // MorseToEnglish();
-
-    // for loop for testing purpose only
-    /*
-    cout << "Testing: \n";
-    for (int i = 0; i < userInput.length(); i++) {
-
-        cout << userInput.at(i) << " is ";// output each letter with its morse reference
-
-        int findPosit = englishRef.find(userInput.at(i));
-        if (findPosit+1)
-            cout << morseRef[findPosit] << endl;
-        else
-            cout << "Not in the reference" << endl;
-    }
-    cout << "------------------------\n\n";
-
-    */
+    //greeting();
+    //userMenu();
+    //EnglishToMorse();
+    MorseToEnglish();
 }//end main
